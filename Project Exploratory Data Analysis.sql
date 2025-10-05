0fef73a17d890e65bd8d35be012729891e757348
mohamed bader <140951121+moba55@users.noreply.github.com>
2025-10-05 23:19:18 +0300

    Add files via upload

---
+++ Project Exploratory Data Analysis.sql
@@ -0,0 +1,107 @@
+SELECT * 
+FROM layoffs_staging2;
+
+
+SELECT MAX(total_laid_off)
+FROM layoffs_staging2;
+
+
+-- serach max and min value  1: all employee laid off // 0: no one laid off
+SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
+FROM layoffs_staging2
+WHERE  percentage_laid_off IS NOT NULL;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE  percentage_laid_off = 1;
+-- meaning all employee leave the company
+
+-- we will order by funds_raised_millions to see the big company
+SELECT *
+FROM layoffs_staging2
+WHERE  percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+-- i have idea about big comoany here
+
+
+-- companies with the most total layoffs
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+
+-- number of layoffs depend on location
+SELECT location, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC 
+LIMIT 10;
+
+
+-- the data order by years
+SELECT YEAR(date), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 ASC;
+
+
+-- the data order by industry
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+-- the data order by stage
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+
+
+
+-- we looked at companies with the hight Layoffs now lets look at that per year
+
+-- using CTE
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
+)
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+
+
+
+
+
+-- Rolling Total of Layoffs Per Month
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
+
+-- now use it in a CTE so we can query off of it
+WITH DATE_CTE AS 
+(
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM DATE_CTE
+ORDER BY dates ASC;
